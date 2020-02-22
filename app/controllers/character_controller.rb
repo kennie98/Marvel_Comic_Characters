e3230887bf48db4b341fc77988c75e8adedc0e50
Ken Tsoi <kennie98@gmail.com>
2020-02-22 18:11:45 -0500

    Finish basic functions.

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,19 +1,28 @@
+$search_term = "all"
 class CharacterController < ApplicationController
   def index
-  	@search_term = params[:looking_for] || "all"
+  	$search_term = params[:looking_for] || "all"
 
-  	case @search_term
+  	case $search_term
 		when "all"
 			@result = Character.all
 		when "events"
 			@result = Character.events
 		else
-			@result = Character.search(@search_term)
+			@result = Character.search($search_term)
 	end
 
-  	@characters = JSON.parse(@result.body || "{}")["data"]["results"]  	
+  	@characters = JSON.parse(@result.body || "{}")["data"]["results"]	
   end
 
   def detail
+  	@search_id = params[:search_id]
+  	case $search_term
+		when "events"
+			@result = Character.searchEvents(@search_id)
+		else
+			@result = Character.search(@search_id)
+	end  	
+  	@character = JSON.parse(@result.body || "{}")["data"]["results"][0]
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -9,11 +9,15 @@ class Character
 		get ("/characters?ts=0218&apikey=93d15baaeb3ee61b58f83eb0f2f46a98&hash=2a7313568f11eb930be4bc3cfb438a33")
 	end
 
-	def self.search(name)
-		get ("/characters/#{name}?ts=0218&apikey=93d15baaeb3ee61b58f83eb0f2f46a98&hash=2a7313568f11eb930be4bc3cfb438a33")
+	def self.search(id)
+		get ("/characters/#{id}?ts=0218&apikey=93d15baaeb3ee61b58f83eb0f2f46a98&hash=2a7313568f11eb930be4bc3cfb438a33")
 	end
 
 	def self.events()
 		get ("/events?ts=0218&apikey=93d15baaeb3ee61b58f83eb0f2f46a98&hash=2a7313568f11eb930be4bc3cfb438a33")
 	end
+
+	def self.searchEvents(id)
+		get ("/events/#{id}?ts=0218&apikey=93d15baaeb3ee61b58f83eb0f2f46a98&hash=2a7313568f11eb930be4bc3cfb438a33")
+	end	
 end
